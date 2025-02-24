cce5f1250e82778c42aa643b2c58724520bfa9af
Anjani <anji.bygani@gmail.com>
2025-02-24 21:13:14 +0530

    updated

--- src/test/java/demo/FirstTest.java
+++ src/test/java/demo/FirstTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class FirstTest {
@@ -13,7 +14,11 @@ public class FirstTest {
 	public void LoginPage()
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver-win64\\Chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--window-size=1920x1080");
+		WebDriver driver = new ChromeDriver(options);
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
